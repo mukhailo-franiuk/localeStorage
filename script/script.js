498b7ea0a2d58fefddb4edb0810e4f8a4bbf512e
mukhailo-franiuk <vipersmf@gmail.com>
2023-02-07 20:36:46 +0200

    Change options

--- script/script.js
+++ script/script.js
@@ -12,12 +12,10 @@ document.getElementById('f-name').addEventListener('input', function () {
     let testFirstName = checkNames.test(document.querySelector('#f-name').value);
     if (testFirstName) {
         document.querySelector('#f-name').style.borderColor = '#008000';
-        document.querySelector('#f-name').setAttribute('name','true');
         statusFirstName = true;
     }
     else {
         document.querySelector('#f-name').style.borderColor = '#ff0000';
-        document.querySelector('#f-name').removeAttribute('name');
         statusFirstName = false;
     }
 });
@@ -25,33 +23,31 @@ document.querySelector('#s-name').addEventListener('input', function () {
     let testSecondName = checkNames.test(document.querySelector('#s-name').value);
     if (testSecondName) {
         document.querySelector('#s-name').style.borderColor = '#008000';
-        document.querySelector('#s-name').setAttribute('name','true');
         statusSecondName = true;
     }
     else {
         document.querySelector('#s-name').style.borderColor = '#ff0000';
-        document.querySelector('#s-name').removeAttribute('name')
         statusSecondName = false;
     }
 });
-document.querySelector('#email-reg').addEventListener('input',function(){
+document.querySelector('#email-reg').addEventListener('input', function () {
     let testEmail = checkEmail.test(document.querySelector('#email-reg').value);
-    if(testEmail){
+    if (testEmail) {
         document.querySelector('#email-reg').style.borderColor = '#008000';
         statusEmail = true;
     }
-    else{
+    else {
         document.querySelector('#email-reg').style.borderColor = '#ff0000';
         statusEmail = false;
     }
 });
-document.querySelector('#pass-reg').addEventListener('input',() => {
+document.querySelector('#pass-reg').addEventListener('input', () => {
     let testEmail = checkPassword.test(document.querySelector('#pass-reg').value);
-    if(testEmail){
+    if (testEmail) {
         document.querySelector('#pass-reg').style.borderColor = '#008000';
-statusPassword = true;
+        statusPassword = true;
     }
-    else{
+    else {
         document.querySelector('#pass-reg').style.borderColor = '#ff0000';
         statusPassword = false
     }
@@ -72,10 +68,10 @@ document.querySelector('#registr').addEventListener('click', () => {
             password: document.querySelector('#pass-reg').value
         });
         localStorage.setItem('users', JSON.stringify(users));
-            document.querySelector('#f-name').value = '';
-            document.querySelector('#s-name').value = '';
-            document.querySelector('#email-reg').value = '';
-            document.querySelector('#pass-reg').value = '';
+        document.querySelector('#f-name').value = '';
+        document.querySelector('#s-name').value = '';
+        document.querySelector('#email-reg').value = '';
+        document.querySelector('#pass-reg').value = '';
     }
 })
 
