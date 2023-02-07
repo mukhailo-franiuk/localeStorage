416c48fd8aee01b0d7957b1673a6485d9d453a3b
mukhailo-franiuk <vipersmf@gmail.com>
2023-02-07 20:27:25 +0200

    Change inputs options

--- script/script.js
+++ script/script.js
@@ -2,18 +2,22 @@
 let checkNames = /^[a-zA-Z0-9]{3,10}$/;
 let checkEmail = /^[a-zA-Z0-9.\-]{1,}\@[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}$/;
 let checkPassword = /^[a-zA-Z0-9\@.,\/]{4,}$/;
+
 let statusFirstName;
 let statusSecondName;
 let statusEmail;
 let statusPassword;
+
 document.getElementById('f-name').addEventListener('input', function () {
     let testFirstName = checkNames.test(document.querySelector('#f-name').value);
     if (testFirstName) {
         document.querySelector('#f-name').style.borderColor = '#008000';
+        document.querySelector('#f-name').setAttribute('name','true');
         statusFirstName = true;
     }
     else {
         document.querySelector('#f-name').style.borderColor = '#ff0000';
+        document.querySelector('#f-name').removeAttribute('name');
         statusFirstName = false;
     }
 });
@@ -21,10 +25,12 @@ document.querySelector('#s-name').addEventListener('input', function () {
     let testSecondName = checkNames.test(document.querySelector('#s-name').value);
     if (testSecondName) {
         document.querySelector('#s-name').style.borderColor = '#008000';
+        document.querySelector('#s-name').setAttribute('name','true');
         statusSecondName = true;
     }
     else {
         document.querySelector('#s-name').style.borderColor = '#ff0000';
+        document.querySelector('#s-name').removeAttribute('name')
         statusSecondName = false;
     }
 });
@@ -39,25 +45,23 @@ document.querySelector('#email-reg').addEventListener('input',function(){
         statusEmail = false;
     }
 });
-document.querySelector('#pass-reg').addEventListener('input',function(){
+document.querySelector('#pass-reg').addEventListener('input',() => {
     let testEmail = checkPassword.test(document.querySelector('#pass-reg').value);
     if(testEmail){
         document.querySelector('#pass-reg').style.borderColor = '#008000';
-        statusPassword = true;
+statusPassword = true;
     }
     else{
         document.querySelector('#pass-reg').style.borderColor = '#ff0000';
-        statusPassword = false;
+        statusPassword = false
     }
-});
-
-
-
+})
 
 
 let users = [];
-document.querySelector('#registr').addEventListener('click', function () {
-    if (statusFirstName && statusSecondName) {
+document.querySelector('#registr').addEventListener('click', () => {
+    if (statusFirstName && statusSecondName && statusEmail && statusPassword) {
+        console.log('work')
         if (localStorage.length > 0 && localStorage.getItem('users')) {
             users = JSON.parse(localStorage.getItem('users'));
         }
